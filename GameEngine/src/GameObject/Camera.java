51b16170196655d31f7feb091e2c71510f67bfeb
Michael <michael.alvarado1@upr.edu>
2021-01-15 22:20:38 -0400

    Camera Bound fixed

--- GameEngine/src/GameObject/Camera.java
+++ GameEngine/src/GameObject/Camera.java
@@ -36,13 +36,33 @@ public class Camera implements GameObject{
 	public void render(Graphics g) {
 		BufferedImage background = this.background.getSubimage(x, y, width, height);
 		g.drawImage(background,0,0,Handler.getWidth(),Handler.getHeight(),null);
+		
+		//Draw Focal Point
+//		Rectangle bound = new Rectangle(x+(focalWidth/2), y+(focalHeight/2), this.focalWidth, this.focalHeight);
+//		g.drawRect(bound.x, bound.y, focalWidth, focalHeight);
 	}
 	
 	private void UpdateCamera() {
-		Rectangle bound = new Rectangle(x+(width/2), y+(height/2), this.focalWidth, this.focalHeight);
+		Rectangle bound = new Rectangle(x+(focalWidth/2), y+(focalHeight/2), this.focalWidth, this.focalHeight);
 		if(!bound.contains(player.x, player.y)) {
 			this.x = player.x;
 			this.y = player.y;
+		}
+		boundStop();
+	}
+	
+	private void boundStop() {
+		if(x<0) {
+			x = 0;
+		}
+		if(x+this.width>background.getWidth()) {
+			x = background.getWidth()-this.width;
+		}
+		if(y<0) {
+			y = 0;
+		}
+		if(y+this.height>background.getHeight()) {
+			y = background.getHeight()-this.height;
 		}
 	}
 }
